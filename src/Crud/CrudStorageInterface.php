7f68dac98ba23e2182194e74a44b7aeb63ea3e26
Tino Ehrich <tino@bigpun.me>
2015-10-09 13:36:17 +0200

    removed CRUD methods from interface

--- src/Crud/CrudStorageInterface.php
+++ src/Crud/CrudStorageInterface.php
@@ -2,8 +2,6 @@
 
 namespace Simplon\Mysql\Crud;
 
-use Simplon\Mysql\MysqlException;
-
 /**
  * Interface CrudStorageInterface
  * @package Simplon\Mysql\Crud
@@ -24,40 +22,4 @@ interface CrudStorageInterface
      * @return CrudModelInterface
      */
     public function getModel();
-
-    /**
-     * @param CrudModelInterface $model
-     *
-     * @return CrudModelInterface
-     */
-    public function create(CrudModelInterface $model);
-
-    /**
-     * @param array $conds
-     *
-     * @return null|CrudModelInterface[]
-     */
-    public function read(array $conds);
-
-    /**
-     * @param array $conds
-     *
-     * @return null|CrudModelInterface
-     */
-    public function readOne(array $conds);
-
-    /**
-     * @param CrudModelInterface $model
-     *
-     * @return CrudModelInterface
-     * @throws MysqlException
-     */
-    public function update(CrudModelInterface $model);
-
-    /**
-     * @param array $conds
-     *
-     * @throws MysqlException
-     */
-    public function delete(array $conds);
 }
