2410afaa7601c08b6b4d1e6316ca3fa937094be5
Tino Ehrich <tino@bigpun.me>
2017-06-16 13:33:37 +0200

    added storage util and helper dep

---
+++ src/Utils/StorageUtil.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Simplon\Mysql\Utils;
+
+use Simplon\Helper\SecurityUtil;
+use Simplon\Mysql\Crud\CrudStoreInterface;
+use Simplon\Mysql\QueryBuilder\ReadQueryBuilder;
+
+/**
+ * @package Simplon\Mysql\Utils
+ */
+class StorageUtil
+{
+    /**
+     * @param CrudStoreInterface $storage
+     * @param string $tokenColumnName
+     * @param int $length
+     * @param string $prefix
+     *
+     * @return string
+     */
+    public static function getUniquePubToken(CrudStoreInterface $storage, string $tokenColumnName = 'pub_token', int $length = 12, ?string $prefix = null)
+    {
+        $token = null;
+        $isUnique = false;
+        $characters = SecurityUtil::TOKEN_UPPERCASE_LETTERS_NUMBERS;
+
+        while ($isUnique === false)
+        {
+            $token = SecurityUtil::createRandomToken($length, $prefix, $characters);
+            $isUnique = $storage->readOne((new ReadQueryBuilder())->addCondition($tokenColumnName, $token)) === null;
+        }
+
+        return $token;
+    }
+}
