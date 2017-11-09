4f8193ce428b2a72f92faee45460e1bf39e95ed7
Tino Ehrich <tino@bigpun.me>
2017-11-09 20:56:36 +0100

    added helper method to set empty tokens

--- src/Utils/StorageUtil.php
+++ src/Utils/StorageUtil.php
@@ -4,6 +4,7 @@ namespace Simplon\Mysql\Utils;
 
 use Simplon\Helper\SecurityUtil;
 use Simplon\Mysql\Crud\CrudStoreInterface;
+use Simplon\Mysql\QueryBuilder\CreateQueryBuilder;
 use Simplon\Mysql\QueryBuilder\ReadQueryBuilder;
 
 class StorageUtil
@@ -36,5 +37,28 @@ class StorageUtil
         }
 
         return $token;
+    }
+
+    /**
+     * @param CreateQueryBuilder $builder
+     * @param string $token
+     *
+     * @return CreateQueryBuilder
+     */
+    public static function autosetEmptyToken(CreateQueryBuilder $builder, string $token): CreateQueryBuilder
+    {
+        $model = $builder->getModel();
+
+        if (method_exists($model, 'getToken') && method_exists($model, 'setToken'))
+        {
+            if (!$model->getToken())
+            {
+                $builder->setModel(
+                    $model->setToken($token)
+                );
+            }
+        }
+
+        return $builder;
     }
 }
